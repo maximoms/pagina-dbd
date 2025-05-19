de24591eadfbf64425c535ba5c490ac13c8daaab
maximoms <92178967+maximoms@users.noreply.github.com>
2025-05-19 14:12:12 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+ScrollReveal().reveal('.mb-3', {
+    duration: 1000,
+    distance: '50px',
+    origin: 'bottom',
+    easing: 'ease-out',
+    reset: true
+  });
+  ScrollReveal().reveal('.boton', {
+    duration: 1000,
+    distance: '50px',
+    origin: 'bottom',
+    easing: 'ease-out',
+    reset: true
+  });
+
